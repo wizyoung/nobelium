06d275c21a15639d003b813ccd3a18dc459c8659
wizyoung <happyyanghehe@gmail.com>
2021-10-03 12:03:10 +0800

    update config and header svg

--- blog.config.js
+++ blog.config.js
@@ -8,7 +8,7 @@
   link: 'https://wizyoung.vercel.app',
   description:
     '',
-  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
