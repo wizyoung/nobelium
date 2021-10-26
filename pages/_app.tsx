370d38fcc573460a54e629f793b094b479f0a16c
wizyoung <happyyanghehe@gmail.com>
2021-10-27 01:52:00 +0800

    fix prismjs import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,15 @@ import 'katex/dist/katex.min.css';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+import 'prismjs';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-cpp';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-typescript';
 import 'prismjs/themes/prism.css';
-import 'prismjs/components/prism-markup'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-bash'
-import 'prismjs/components/prism-python'
-import 'prismjs/components/prism-c'
-import 'prismjs/components/prism-cpp'
 import 'react-notion-x/src/styles.css';
 import 'react-static-tweets/styles.css';
 import BLOG from '~/blog.config';
