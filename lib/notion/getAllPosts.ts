79704ab4b22d0a478add219917d9b94888e5a1e0
wizyoung <happyyanghehe@gmail.com>
2021-10-31 02:42:40 +0800

    fix components and css

--- lib/notion/getAllPosts.ts
+++ lib/notion/getAllPosts.ts
@@ -58,7 +58,6 @@ const returnGetAllPosts = async ({
     for (let i = 0; i < pageIds.length; i++) {
       const id = pageIds[i];
       const properties = (await getPageProperties(id, block, schema)) || null;
-
       // Add fullwidth, createdtime to properties
       properties.createdTime = new Date(block[id].value?.created_time).toString();
       properties.fullWidth = (block[id].value?.format as BasePageBlock['format'])?.page_full_width ?? false;
